3b8301de072c9be68af44eeabd620c28e96a3dee
a.h.jafari <amirjaffari300245@gmail.com>
2022-08-29 09:29:20 +0430

    some change

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <meta property="og:title" content="The Rock" />
         <meta property="og:type" content="video.movie" />
         <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
-        <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
+        <meta property="og:image" content="https://diamond-press.com/images/siteLogo/diamond-press.svg" />
         <meta property="og:description"
           content="Sean Connery found fame and fortune as the
          suave, sophisticated British agent, James Bond." />
